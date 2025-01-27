90d80d30fb202ebc5382fd22ff261f52471beb35
GUIFRE88 <guilherme.fre073@gmail.com>
2025-01-27 07:05:11 -0300

    bug: Melhoria de mensagens e adição de dependenvy destroy no model.

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,9 +1,9 @@
 class Invite < ApplicationRecord
-  has_many :company_invites
+  has_many :company_invites, dependent: :destroy
   has_many :companies, through: :company_invites
-  has_many :administrator_company_invites
+  has_many :administrator_company_invites, dependent: :destroy
   has_many :administrators, through: :administrator_company_invites
-  has_many :companies, through: :company_invites
 
   validates :name, :date_completion, presence: true
+
 end
